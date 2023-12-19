b71ff0b0764a41a9e9eeb9ab5ab7ecdb64d41bc7
Hanneman616 <juanpablo000@hotmail.es>
2023-12-18 22:26:13 -0500

    javascript

---
+++ js/test-script.js
@@ -0,0 +1,18 @@
+function suma(a, b) {
+    return a + b;
+}
+
+console.log('Hello World!' + suma(2, 3));
+
+
+
+
+//QUE SON LAS VARIABLES MUTALES E INMUTABLES
+//
+//Las variables mutables son aquellas que pueden cambiar su valor en cualquier momento, mientras que las inmutables son aquellas que no pueden cambiar su valor.    q
+
+
+//QUE SON LAS VARIABLES POR VALOR Y POR REFERENCIA?
+//
+//
+
