7fd2c406917fa9ab7ad4e4bc4470b4bcdb2820f1
Hanneman616 <juanpablo000@hotmail.es>
2024-01-07 21:08:58 -0500

    ajax , jquery, clipboard..

--- js/test-script.js
+++ js/test-script.js
@@ -4,8 +4,31 @@ function suma(a, b) {
 
 console.log('Hello World!' + suma(2, 3));
 
+initialize();
+
+function initialize() {
+    
+    console.log('OnLoad Complete');
+    console.log(navigator.userAgent);
+    console.log(navigator.language);
+    console.log(navigator.languages);
+
+}
 
 
+function copiarTexto() {
+    // Obtener el elemento de entrada de texto
+    const textoInput = document.getElementById('textoInput');
+
+    // Intentar copiar el texto al portapapeles
+    navigator.clipboard.writeText(textoInput.value)
+      .then(() => {
+        alert('Texto copiado exitosamente al portapapeles');
+      })
+      .catch(err => {
+        console.error('Error al intentar copiar al portapapeles:', err);
+      });
+  }
 
 //QUE SON LAS VARIABLES MUTALES E INMUTABLES
 //
